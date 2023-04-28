77fd590d7a8555128c2ae2ccbbccdebfba2ae2bc
Hoomaan <id@hoomaan.dev>
2023-04-28 10:24:33 +0400

    Initial commit

---
+++ eden.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Eden
+Plugin URI: https://eden.bz/
+Description: Useful functions created by Eden Design & Digital Solutions
+Version: 1.0
+Author: Hamidreza(Hoomaan) Sheikholeslami
+Author URI: https://hoomaan.dev/
+License: GPLv2 or later
+Text Domain: eden
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
