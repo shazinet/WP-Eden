f055f29a6aa0753a1e6667609919da8430d2ef5c
Hoomaan <id@hoomaan.dev>
2023-05-01 12:47:25 +0400

    Updated the plugin name

--- wp-eden.php
+++ wp-eden.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: Eden
+Plugin Name: WP Eden
 Plugin URI: https://eden.bz/
 Description: Useful functions created by Eden Design & Digital Solutions
 Version: 1.0.0
 Author: Hamidreza(Hoomaan) Sheikholeslami
 Author URI: https://hoomaan.dev/
 License: GPLv2 or later
-Text Domain: eden
+Text Domain: wp-eden
 */
 
 // If this file is called directly, abort.
